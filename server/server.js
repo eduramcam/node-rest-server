fbfa37c5b2cfca42ad3261553849324b9090cd9c
Eduardo Ramirez Camacho <er546a@att.com>
2019-10-15 17:32:13 -0500

    Actualizacion de server js para version

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
 
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
         if (err) throw err;
         console.log('Base de datos ONLINE');
